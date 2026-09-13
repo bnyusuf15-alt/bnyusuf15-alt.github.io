617a654e05ad7bc6057a4a0986a2c192a3f47f5f
bnyusuf15-alt <bnyusuf15@gmail.com>
2026-09-13 10:06:05 +0300

    Update festvcannes.py

--- FR/festvcannes.py
+++ FR/festvcannes.py
@@ -8,15 +8,13 @@ output_file = "FR/festvcannes.m3u8"
 response = requests.get(playlist_url)
 response.raise_for_status()
 
-lines = response.text.splitlines()
-
 stream_url = response.text
 
 os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
 with open(output_file, "w", encoding="utf-8") as f:
-    f.write(#EXTM3U)
-    f.write(#EXT-X-STREAM-INF:BANDWIDTH=7680000)
+    f.write("#EXTM3U\n")
+    f.write("#EXT-X-STREAM-INF:BANDWIDTH=7680000\n")
     f.write(stream_url)
 
 print(f"Saved to {output_file}")
