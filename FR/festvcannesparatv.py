6bbaa580b916d1828584e99c542ca9f320c9a3a0
bnyusuf15-alt <bnyusuf15@gmail.com>
2026-09-13 10:16:45 +0300

    Update festvcannesparatv.py

--- FR/festvcannesparatv.py
+++ FR/festvcannesparatv.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 playlist_url = "https://hdfauth.ftven.fr/esi/TA?url=https://live-event.ftven.fr/out/v1/64bfbfd7f96c40b3989b029663be3e6d/index_france-domtom.m3u8"
-output_file = "FR/festvcannes.m3u8"
+output_file = "FR/festvcannesparatv.m3u8"
 
 # Download playlist
 response = requests.get(playlist_url)
